34c9f68f50ef72f0c06f08c7fbc9d348ff085d44
Dana Jackson <danajackson2@gmail.com>
2020-10-13 16:19:08 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,15 @@
 def find_max_value(array)
-  array.sort.pop
-end
+  i = 0
+  max_value = -1
+  while i<array.length do
+    if array[i] > max_value 
+      max_value = array[i]
+    end  
+  end  
+  max_value
+end  
+
+
+#def find_max_value(array)
+#  array.sort.pop
+#end
